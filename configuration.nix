88a30b3a52eeebf58cd48442fd8a16f5e2e8d384
Michael Mercier <michael.mercier@inria.fr>
2017-08-21 14:38:42 +0200

    Smart card + GDM + proxy

--- configuration.nix
+++ configuration.nix
@@ -34,7 +34,7 @@
 
   # Set your time zone.
   time.timeZone = "Europe/Paris";
-  
+
   nixpkgs.config.allowUnfree = true;
 
   # Add virtualbox
@@ -65,18 +65,26 @@
     gnupg
     pass
     zsh
+    nix-zsh-completions
+
     ## Graphical environment
     libreoffice
     transmission_gtk
     gitg
     firefox
     gnome3.evolution
+    usbutils
+
     # Development environment
     gcc
     ctags
     gnumake
     wget
     cmake
+
+    # Pro
+    cntlm
+    opensc
   ];
 
   # use Vim by default
@@ -112,7 +120,7 @@
       xkbOptions = "eurosign:e";
       # Enable the Gnome Desktop Environment.
       desktopManager.gnome3.enable = true;
-      #displayManager.gdm.enable = true;
+      displayManager.gdm.enable = true;
     };
 
   };
@@ -133,8 +141,13 @@
 
   # Networking configuration
   services.polipo.enable = true;
-  networking.proxy.default = "http://127.0.0.1:8123";
-  # TODO add an other polipo conf with parent proxy "http://193.56.47.8:8080";
+  services.polipo.parentProxy = "193.56.47.8:8080";
+
+  networking.proxy.httpProxy = "http://127.0.0.1:8123";
+  networking.proxy.httpsProxy = "http://127.0.0.1:8123";
+  networking.proxy.noProxy = "127.0.0.1,localhost,::1";
+
+  # TODO override polipo conf with parent proxy "http://193.56.47.8:8080";
 
   # FIXME (not sure if it is necessary...
   # Enable system wide zsh and ssh agent
@@ -148,12 +161,16 @@
     description = "Michael Mercier";
     uid = 1000;
     isNormalUser = true;
-    extraGroups = [ "wheel" "lp" "networkmanager" ];
+    extraGroups = [ "wheel" "lp" "networkmanager" "vboxusers" "gdm" ];
     shell = pkgs.zsh;
     openssh.authorizedKeys.keys = [
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDXXsbhzexlZS+hCRS39cEo3BOZTLGtO8mKUE6U0gwDRNE0hE/H4Nr9Gn9NGxmmWFdTZmuaU85Z66ofe4C9Hz8pgRzB8b4Ids3XXzdeud5+znN7tKERt9Qi7a25q8vKTYlftcOIvT+6v4YVmih/NoLx5Dw9+B2tS0E20VY0skbL4s7TYmrXIrSPz6dX/JpjivF90eJSc0pVkT8ZSbZV2fygRW7JbgN490iJ+sRGnwfDjpYA7yQjyrpDJifXwdXg/NnilW6WnwbKBNXcjTIsVg1PEffZIJKAEOZzl/txNKU2kjYAZTtBGqJ491md+T3ptjvcNehia5if3GGbjm7xalzsRwOVmMisfQT4KP/cyZ66fxbU8qMgzspCjvCpEPXRQH2Q4jVWZkx2iulmB2wNbXaxs58ueelMJMO3gpZ2xBYwah7QSZK2nHG44dKfC6OAGcah7FI5+dplzwBtROWvPxkiSWJbBcxymcY5QkValWJeavC7gwDhC/zjNfa+oKRLYSsgoiiD0BRcvm+UisyGZ59C3T0lJTZpnn63RwY4WvVQzh1ltdqckZMFwaqn0ywIA9+JCD2u9P8STiRpcXq+kQnEMPYUIXRGm8KFXkdB08j8uNC0F+EF7oqgWTpGv8xVJnic48V49Vp9DDIK4BCuqgViwBMBaIqosX3j9E8JzWuAnQ== mercierm@oursbook"];
   };
 
+
+  # Smart card
+  services.pcscd.enable = true;
+  
 
   # The NixOS release to be compatible with for stateful data such as databases.
   system.stateVersion = "17.03";
