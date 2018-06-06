984fa8af2e458992860db0d0b695f0831e4b0b65
MERCIER Michael <michael.mercier@inria.fr>
2018-06-06 17:23:52 +0200

    Add echo cancelation with pulseaudio

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -58,11 +58,16 @@
     # NixOS allows either a lightweight build (default) or full build of PulseAudio to be installed.
     # Only the full build has Bluetooth support, so it must be selected here.
     package = pkgs.pulseaudioFull;
+
+    # Add echo-cancelation virtual interface
+    extraConfig = ''
+      load-module module-echo-cancel
+    '';
   };
-  hardware.bluetooth.extraConfig = "
+  hardware.bluetooth.extraConfig = ''
     [general]
     Enable=Source,Sink,Media,Socket
-  ";
+  '';
 
   hardware.bluetooth.enable = true;
 }
