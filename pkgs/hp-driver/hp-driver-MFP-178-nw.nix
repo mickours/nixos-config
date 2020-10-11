c9dd5221cd59a5e8ea178c535667449183f9463a
Michael Mercier <michael.mercier@ryax.tech>
2020-10-11 19:27:55 +0200

    Driver now working

--- pkgs/hp-driver/hp-driver-MFP-178-nw.nix
+++ pkgs/hp-driver/hp-driver-MFP-178-nw.nix
@@ -1,6 +1,8 @@
-{ stdenv, glibc, gcc, libusb, patchelf, cups, ghostscript}:
+{ stdenv, glibc, libusb, cups }:
 let
-  cups' = stdenv.lib.getLib cups;
+  installationPath = if stdenv.hostPlatform.system == "x86_64-linux" then "x86_64" else "i386";
+  appendPath = if stdenv.hostPlatform.system == "x86_64-linux" then "64" else "";
+  libPath = stdenv.lib.makeLibraryPath [ cups libusb ] + ":$out/lib:${stdenv.cc.cc.lib}/lib${appendPath}";
 in stdenv.mkDerivation rec {
   name = "HP-MFP-178-nw-driver-${version}";
   version = "1.00.39.12_00.15";
@@ -9,11 +11,45 @@ in stdenv.mkDerivation rec {
     sha256 = "02h4if0zx7x9fnfa0na4kpz1y1g3838wa9shjmrcqzs5wmpilrcf";
   };
 
-  nativeBuildInputs = [ patchelf ];
-  buildInputs = [ glibc gcc libusb cups ghostscript ];
-  inherit glibc gcc libusb cups ghostscript;
+  dontPatchELF = true;
+  dontStrip = true;
 
-  builder = ./builder.sh;
+  installPhase = ''
+    cd ${installationPath}
+    mkdir -p $out/lib/cups/{backend,filter}
+    install -Dm755 smfpnetdiscovery $out/lib/cups/backend/
+    install -Dm755 pstosecps rastertospl $out/lib/cups/filter/
+    install -Dm755 libscmssc.so $out/lib/
+    GLOBIGNORE=*.so
+    for exe in $out/lib/cups/**/*; do
+      echo "Patching $exe"
+      patchelf \
+        --set-rpath ${libPath} \
+        --set-interpreter $(cat $NIX_CC/nix-support/dynamic-linker) \
+        $exe
+    done
+    unset GLOBIGNORE
+    install -v libsane-smfp.so.1.0.1 $out/lib
+    cd $out/lib
+    ln -s -f libsane-smfp.so.1.0.1 libsane-smfp.so.1
+    ln -s -f libsane-smfp.so.1 libsane-smfp.so
+    for lib in $out/lib/*.so; do
+      echo "Patching $lib"
+      patchelf \
+        --set-rpath ${libPath} \
+        $lib
+    done
+    mkdir -p $out/share/cups/model/hp
+    cd -
+    cd ../noarch/share/ppd/
+    for i in *.ppd; do
+      sed -i $i -e \
+        "s,pstosecps,$out/lib/cups/filter/pstosecps,g; \
+         s,pstospl,$out/lib/cups/filter/pstospl,g; \
+         s,rastertospl,$out/lib/cups/filter/rastertospl,g"
+    done;
+    cp -r ./* $out/share/cups/model/hp
+  '';
 
   meta = with stdenv.lib; {
     description = "HP driver for MFP178-nw";
